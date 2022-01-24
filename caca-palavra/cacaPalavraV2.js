2dbc315c0d9a4600d13f572dffae684d6bade71d
Thiago Pereira <thiagopereiramail@gmail.com>
2022-01-24 20:49:00 +0000

    caca palavra da direita para esquerda

--- caca-palavra/cacaPalavraV2.js
+++ caca-palavra/cacaPalavraV2.js
@@ -1,6 +1,7 @@
 // Azul
-// (0, 7)
-// (4, 0)
+// [0, 7]
+// [4, 0]
+// [6, 11]
 
 const grid = [
   ["A", "B", "A", "C", "A", "T", "E", "A", "Z", "U", "L", "P", "E"], // 0
@@ -11,7 +12,7 @@ const grid = [
   ["Z", "D", "K", "S", "Z", "S", "A", "O", "D", "A", "K", "D", "D"], // 5
   ["U", "O", "T", "S", "L", "U", "A", "O", "L", "U", "Z", "A", "P"], // 6
   ["L", "O", "T", "T", "T", "R", "Z", "L", "P", "S", "K", "D", "D"], // 7
-];
+]; //0   1    2    3    4    5    6    7    8   9     10   11  12
 
 const ocorrencias = [];
 
@@ -19,6 +20,16 @@ function verificaEsquerdaDireita(palavra, matriz, i, j) {
   for (let h = 1; h < palavra.length; h++) {
     const tamanhoLinha = matriz[i].length;
     if (j + h >= tamanhoLinha || palavra[h] != matriz[i][j + h]) {
+      return;
+    }
+  }
+
+  ocorrencias.push([i, j]);
+}
+
+function verificaDireitaEsquerda(palavra, matriz, i, j) {
+  for (let h = 1; h < palavra.length; h++) {
+    if (j - h < 0 || palavra[h] != matriz[i][j - h]) {
       return;
     }
   }
@@ -42,6 +53,7 @@ function verificaOcorrencias(palavra, matriz) {
     for (let j = 0; j < matriz[i].length; j++) {
       if (palavra[0] == matriz[i][j]) {
         verificaEsquerdaDireita(palavra, matriz, i, j);
+        verificaDireitaEsquerda(palavra, matriz, i, j);
         verificaCimaBaixo(palavra, matriz, i, j);
       }
     }
