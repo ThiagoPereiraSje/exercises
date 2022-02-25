291f57a99b58aab66edd22e3f41062605d9e4da6
Thiago Pereira <thiagopereiramail@gmail.com>
2022-02-25 00:30:40 +0000

    exercicio de criptografia

---
+++ criptografia/index.js
@@ -0,0 +1,43 @@
+/*
+Rotacionar
+palavra: Ydprv = vamos
+*/
+
+function descriptograVinegere(text, rotation) {
+  let lower = text.toLowerCase();
+  let resposta = [];
+
+  for (let letra of lower) {
+    let v = letra.charCodeAt();
+
+    if (v < 97 || v > 122) {
+      resposta.push(letra);
+      continue;
+    }
+
+    let valor = letra.charCodeAt() - rotation;
+
+    if (valor < 97) {
+      valor += 26;
+    }
+
+    resposta.push(String.fromCharCode(valor));
+  }
+
+  return resposta.join("");
+}
+
+// const resposta = descriptografaCesar("Ydprv", 3);
+// const resposta = descriptografaCesar("aaaa", 3);
+const primeira = descriptograVinegere("Ydprv dsuhqghu fulswrjudild", 3);
+const segunda = descriptograVinegere("Ydl sud flpd ghohv Jdor", 3);
+const terceira = descriptograVinegere("Ibpr + Pbzhavqnqr = CbqPbqne", 13);
+const quarta = descriptograVinegere(
+  "Yrzoerz-fr qn ncerfragnpnb qr svany qr pvpyb frznan dhr irz",
+  13
+);
+
+console.log(primeira);
+console.log(segunda);
+console.log(terceira);
+console.log(quarta);
